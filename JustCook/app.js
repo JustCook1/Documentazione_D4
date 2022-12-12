ac74349027c302eb0dc5c4d95a9644fd0b905971
giulio <giulio.bazzoli@studenti.unitn.it>
2022-12-12 17:24:01 +0100

    "aggiunto swagger

--- JustCook/app.js
+++ JustCook/app.js
@@ -1,4 +1,11 @@
 require('dotenv').config()
 
+//codice per aggiungere swagger
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
