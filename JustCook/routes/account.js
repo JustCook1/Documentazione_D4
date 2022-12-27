8b00a8c7de119da2c05380f19231ea429c953f53
MocDenny <denise.comincioli@studenti.unitn.it>
2022-12-27 14:50:57 +0100

    sistemazione route

--- JustCook/routes/account.js
+++ JustCook/routes/account.js
@@ -2,5 +2,9 @@ const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/account')
 
+router.patch('/aggiungiAiPreferiti', accountController.aggiungiAiPreferiti);
+router.patch('/togliDaiPreferiti', accountController.togliDaiPreferiti);
+router.patch('/aggiungiRating', accountController.aggiungiRating);
+router.patch('/completaRicetta', accountController.completaRicetta);
 
 module.exports = router;
--- JustCook/routes/ricettaEstesa.js
+++ JustCook/routes/ricettaEstesa.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router()
-const accountController = require('../controllers/account');
 const ricettaEstesaController = require('../controllers/ricettaEstesa');
 
 
 router.get('/cercaRicette/:cerca', ricettaEstesaController.cercaRicette);
-router.patch('/aggiungiAiPreferiti', accountController.aggiungiAiPreferiti);
-router.patch('/togliDaiPreferiti', accountController.togliDaiPreferiti);
-router.patch('/aggiungiRating', accountController.aggiungiRating);
-router.patch('/completaRicetta', accountController.completaRicetta);
 
 
 module.exports = router; 
