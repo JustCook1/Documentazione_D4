73bd8601bddcadd7b8ce0b3d458b79a774bb1fd4
giulio <giulio.bazzoli@studenti.unitn.it>
2022-12-31 14:18:25 +0100

    aggiornata funzione nuovapassword

---
+++ JustCook/grafica/script_cambiapass.js
@@ -0,0 +1,26 @@
+//funzione che cambia la password dell'utente
+function cambiaPassword() {
+    let email = document.getElementById("mail").value;
+    let nuovaPassword = document.getElementById("nuovaPassword").value;
+
+    const url = "http://localhost:8080/account/"+email+"/"+nuovaPassword;
+    fetch(url, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.error) throw new Error(data.error);
+        console.log(data);
+        window.location.href = "index.html";
+        alert("Password cambiata con successo");
+        //document.getElementById("mail").disabled = true;
+        //document.getElementById("nuovaPassword").disabled = true;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert("Errore nel cambio della password");
+        return false;
+    }
+    );
+}
